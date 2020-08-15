23ccd13a1ed31c7e10860140c0a5c2f51f3b15f6
Rishabh Jain <54959830+Rishabh-Jain21@users.noreply.github.com>
2020-08-15 08:03:08 +0530

    Update crack.py

--- crack.py
+++ crack.py
@@ -1,16 +1,18 @@
 import zipfile
 from threading import Thread
 
+
 def extract_zip(zfile, password):
     try:
         zfile.extractall("Password Protected123", pwd=bytes(password.encode()))
         print("[+] Password Found: "+password+'\n')
     except Exception as e:
-        print(e)
+        pass
 
-
-zfile = zipfile.ZipFile("test.zip")
-passfile = open('word_list.txt')
+file_path=r"" #Enter file path
+wordlist=r"" #Word list path
+zfile = zipfile.ZipFile(file_path)
+passfile = open(wordlist)
 for line in passfile.readlines():
     password = line.strip('\n')
     t = Thread(target=extract_zip, args=(zfile, password))
