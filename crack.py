80882cdd4f30d4558a0410bd16e36948d6a0882a
Rishabh <Rishabh9843@gmail.com>
2020-12-22 20:57:00 +0530

    updated gitignore and code

--- crack.py
+++ crack.py
@@ -3,14 +3,22 @@ from threading import Thread
 import argparse
 import os
 
+if not os.path.exists(os.path.join('passwords.txt')):
+    with open("passwords.txt", 'w') as f:
+        pass
 
-def extract_zip(file_path, zfile, password):
-    extract_file_name = os.path.splitext(file_path)[0]
+
+def extract_zip(orignal_file_path, extract_file_path, zfile, password):
+    extract_file_name = os.path.splitext(extract_file_path)[0]
     try:
         zfile.extractall(extract_file_name, pwd=bytes(password.encode()))
         print("[+] Password Found: "+password+'\n')
+
+        with open("passwords.txt", 'a') as p:
+            line = f"File Name :{orignal_file_path} Password :{password}\n"
+            p.write(line)
     except Exception as e:
-        print(e)
+        pass
 
 
 parser = argparse.ArgumentParser()
@@ -21,15 +29,17 @@ parser.add_argument('-w', '--word_list', type=str, metavar='',
 parser.add_argument('-e', "--extract_name", metavar='',
                     help='Folder name for extracted files (Default: with zip name)')
 args = parser.parse_args()
-file_path = args.file
+orignal_file_path = args.file
 wordlist = args.word_list
-print(file_path, wordlist)
-zfile = zipfile.ZipFile(file_path)
+zfile = zipfile.ZipFile(orignal_file_path)
+extract_file_path = orignal_file_path
 if args.extract_name:
-    file_path = os.path.join(args.extract_name)
+    folder_path = os.path.dirname(orignal_file_path)
+    extract_file_path = os.path.join(folder_path, args.extract_name)
 
 passfile = open(wordlist)
 for line in passfile.readlines():
     password = line.strip('\n')
-    t = Thread(target=extract_zip, args=(file_path, zfile, password))
+    t = Thread(target=extract_zip, args=(
+        orignal_file_path, extract_file_path, zfile, password))
     t.start()
